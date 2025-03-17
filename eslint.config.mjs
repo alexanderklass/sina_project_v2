535078af5716d0b8e1e5710e40f92b1deed75aed
alexanderklass <alexklass16@gmail.com>
2025-03-17 21:29:02 +0100

    Updating lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
     ...compat.config({
         extends: ['next/core-web-vitals', 'next/typescript'],
-        rules: { 'react/no-unescaped-entities': 'off' },
+        rules: { 'react/no-unescaped-entities': 'off', '@typescript-eslint/no-explicit-any': 'off' },
     }),
 ];
 export default eslintConfig;
