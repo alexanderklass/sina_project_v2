52e4a7063f80b6e2fae8d96203244706ac2c9780
alexanderklass <alexklass16@gmail.com>
2025-06-01 19:26:20 +0200

    changeg config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    /*
     output: 'export',
     basePath: '/sina_project',
     assetPrefix: '/sina_project',
-    */
 };
 
 export default nextConfig;
